6751faea3bbb40ae1be36feb849001430d00866e
Allan Galarza <allan.galarza@gmail.com>
2017-11-12 22:48:42 -0700

    Improved spell attribute parsing

--- tibiawiki-sql.py
+++ tibiawiki-sql.py
@@ -12,6 +12,8 @@ from utils.npcs import fetch_npc_images, fetch_npc_list, fetch_npcs
 from utils.quests import fetch_quests, fetch_quest_list
 from utils.spells import fetch_spell_images, fetch_spells_list, fetch_spells
 
+__version__ = "v0.1.1"
+
 DATABASE_FILE = "tibia_database.db"
 SKIP_IMAGES = "skipimages" in sys.argv
 
